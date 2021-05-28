1fd33b07ac37747d64295a6a352ceb42f8c43530
David Barbosa <david.barbosa@mercadolibre.com.co>
2021-05-27 22:28:27 -0500

    Proposal exercises

---
+++ 20210527_Covid/index.py
@@ -0,0 +1,15 @@
+# El número de contagiados de Covid-19 en el país de NuncaLandia se
+# duplica cada día. Hacer un programa que diga el número total de
+# personas que se han contagiado cuando pasen D días a partir de hoy,
+# si el número de contagiados actuales es C.
+
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+
---
+++ 20210527_Tax/index.py
@@ -0,0 +1,14 @@
+# Si pido prestados P cantidad de pesos para pagarlos en dos meses, si
+# el interés del préstamo es del 3%. ¿Cuánto se debe pagar al final del
+# segundo mes si el interés es compuesto mensualmente?.
+
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+

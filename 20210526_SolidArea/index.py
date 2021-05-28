14cd757446a9ca66e67044072271d9dfeade0af7
David Barbosa <davidbarbosa.dev@gmail.com>
2021-05-27 20:31:58 -0500

    Update index.py

--- 20210526_SolidArea/index.py
+++ 20210526_SolidArea/index.py
@@ -1,21 +1,21 @@
 from math import pi
 
 
-def circularConeVolume(height, radius):
-    return 1/3 * height * pi * radius**2 
+def circular_cone_volume(height, radius):
+    return 1/3 * height * pi * radius**2
 
 
-def sphereVolume(radius):
+def sphere_volume(radius):
     return 4/3 * pi * radius**3
 
 
-def solidVolume(value1, value2):
+def solid_volume(value1, value2):
     return value1 + value2
 
 
-coneRadius = float(input('Cone radius: '))
-coneHeight = float(input('Cone height: '))
-sphereRadius = float(input('Sphere Radius: '))
+cone_radius = float(input('Cone radius: '))
+cone_height = float(input('Cone height: '))
+sphere_radius = float(input('Sphere Radius: '))
 
 print(
-    f"Solid volume: {solidVolume(circularConeVolume(coneHeight, coneRadius), sphereVolume(sphereRadius))}")
+    f"Solid volume: {solid_volume(circular_cone_volume(cone_height, cone_radius), sphere_volume(sphere_radius))}")

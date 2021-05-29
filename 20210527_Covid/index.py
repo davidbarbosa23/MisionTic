e9930b5d069db1854c12996542aa97d2e410193e
David Barbosa <david.barbosa@mercadolibre.com.co>
2021-05-28 19:14:47 -0500

    Resolve exercises

--- 20210527_Covid/index.py
+++ 20210527_Covid/index.py
@@ -1,15 +1,21 @@
+# PROBLEMA
 # El número de contagiados de Covid-19 en el país de NuncaLandia se
 # duplica cada día. Hacer un programa que diga el número total de
 # personas que se han contagiado cuando pasen D días a partir de hoy,
 # si el número de contagiados actuales es C.
 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
+# PSEUDO
+# D = int - Días que quiero estimar
+# C = int - Contagiados actuales
+# PROYECCIÓN: C * (2^D)
 
+# SOLUCIÓN
+def projection(current_infected, days):
+    return current_infected * 2 ** days
+
+
+current_infected = int(input('Contagiados actuales en NuncaLandia: '))
+days = int(input('Días a calcular: '))
+
+print(
+    f"Proyección de contagiados en {days} días: {projection(current_infected, days)}")
--- 20210527_Tax/index.py
+++ 20210527_Tax/index.py
@@ -1,14 +1,19 @@
+# PROBLEMA
 # Si pido prestados P cantidad de pesos para pagarlos en dos meses, si
 # el interés del préstamo es del 3%. ¿Cuánto se debe pagar al final del
 # segundo mes si el interés es compuesto mensualmente?.
 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
+# PSEUDO
+# P = int Dinero prestado
+# M = int Meses a pagar
+# DEUDA CON INTERÉS COMPUESTO = P * (1 + 0.03) ^ M
 
+# SOLUCIÓN
+def compound_interest(debt, months):
+    return debt * 1.03 ** months
+
+
+debt = int(input('Prestamo: '))
+months = int(input('Meses: '))
+
+print(f"Pago total en {months} meses: {compound_interest(debt, months)}")

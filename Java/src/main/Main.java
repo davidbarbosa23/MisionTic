4f5fef5e84ed6dcfee09e0003e634e3af70c5bec
David Barbosa <david.barbosa@mercadolibre.com.co>
2021-08-16 14:37:30 -0500

    add main menu

--- Java/src/main/Main.java
+++ Java/src/main/Main.java
@@ -1,12 +1,32 @@
 package main;
 
-//import tasks.Task1.Baler;
+import tasks.Task1.Baler;
 import tasks.Task2.Libreria;
+
+import java.util.Scanner;
 
 
 public class Main {
+    private static void menu() {
+        int choice;
+        Scanner scan = new Scanner(System.in);
+
+        do {
+            System.out.println("Select task");
+            System.out.println("1. Baler task");
+            System.out.println("2. Library task");
+            System.out.println("0. Out");
+
+            String option = scan.nextLine();
+            choice = Integer.parseInt(option.substring(0, 1));
+
+            if (choice == 1) new Baler();
+            if (choice == 2) new Libreria();
+
+        } while (choice != 0);
+    }
+
     public static void main(String[] args) {
-        //new Baler();
-        new Libreria();
+        menu();
     }
 }

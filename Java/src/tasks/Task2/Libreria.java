a27b14c49143fdb0308d91d6b6f8c3672458dc63
David Barbosa <davidbarbosa.dev@gmail.com>
2021-08-20 11:51:23 -0500

    Change comments in task3 to prevent confusion

--- Java/src/tasks/Task2/Libreria.java
+++ Java/src/tasks/Task2/Libreria.java
@@ -39,66 +39,24 @@ public class Libreria {
             }
         } while (choice != 3);
     }
-
+    
+    /*
+     * This method is just for test with main program
+     */
     public Libreria() {
         items = new ArrayList<>();
         menu();
     }
+    
+    /* 
+     * This commented lines are the methods for Automated Testing 
+     */
+    // public Libreria() {
+    //     items = new ArrayList<>();
+    // }
 
-
+    // public static void main(String[] args) {
+    //     Libreria libreria = new Libreria();
+    //     libreria.menu();
+    // }
 }
-
-/**
- // Submited
-
- import java.util.ArrayList;
- import java.util.List;
- import java.util.Scanner;
-
- public class Libreria {
-
-     List items;
-
-     private int choice = 0;
-     private Scanner scan = new Scanner(System.in);
-
-     public void listLibrary() {
-         for (int i = 0; i < items.size(); i++) {
-            System.out.println(items.get(i).toString());
-         }
-     }
-
-     private void menu() {
-         do {
-             String option = scan.nextLine();
-             choice = Integer.parseInt(option.substring(0, 1));
-
-             if (choice == 1) {
-                 String[] params = option.split("_");
-
-                 if (new String(params[3]).equals("Libro")) {
-                     Libro libro = new Libro(params[1], Integer.parseInt(params[2]), params[3], params[4]);
-                     items.add(libro);
-                 }
-                 if (new String(params[3]).equals("Revista")) {
-                     Revista revista = new Revista(params[1], Integer.parseInt(params[2]), params[3], Integer.parseInt(params[4]));
-                     items.add(revista);
-                 }
-             }
-             if (choice == 2) {
-                 System.out.println("***Publicaciones disponibles***");
-                 listLibrary();
-             }
-         } while (choice != 3);
-     }
-
-     public Libreria() {
-        items = new ArrayList<>();
-     }
-
-     public static void main(String[] args) {
-         Libreria libreria = new Libreria();
-         libreria.menu();
-     }
- }
-*/

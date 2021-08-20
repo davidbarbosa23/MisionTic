2cc8f8a1e20e49e01b1710a1ebc5270201db80b3
David Barbosa <davidbarbosa.dev@gmail.com>
2021-08-20 11:55:35 -0500

    Update Baler.java

--- Java/src/tasks/Task1/Baler.java
+++ Java/src/tasks/Task1/Baler.java
@@ -22,6 +22,9 @@ public class Baler {
             return "cuatro";
     }
 
+    /*
+     * This method is just for test with main program
+     */
     public Baler() {
         Scanner scan = new Scanner(System.in);
 
@@ -35,45 +38,21 @@ public class Baler {
         System.out.println(out);
         System.out.println(category);
     }
+    
+    /*
+     * This commented lines are the method for Automated testing
+     */
+    // public static void main(String[] args) {
+    //     Scanner scan = new Scanner(System.in);
+
+    //     int muscleMass = scan.nextInt();
+    //     int secondTest = second(muscleMass);
+    //     int thirdTest = third(muscleMass, secondTest);
+
+    //     String out = muscleMass + " " + secondTest + " " + thirdTest;
+    //     String category = category(thirdTest);
+
+    //     System.out.println(out);
+    //     System.out.println(category);
+    // }
 }
-
-/**
- // Submited
-
- import java.util.Scanner;
-
- public class Main {
-     public static int second(int muscleMass) {
-        return muscleMass * 2 + 4;
-     }
-
-     public static int third(int muscleMass, int second) {
-        return Math.floorDiv((muscleMass + second), 5);
-     }
-
-     public static String category(int third) {
-         if (third < 20)
-            return "uno";
-         else if (third < 30)
-            return "dos";
-         else if (third < 50)
-            return "tres";
-         else
-            return "cuatro";
-     }
-
-     public static void main(String[] args) {
-         Scanner scan = new Scanner(System.in);
-
-         int muscleMass = scan.nextInt();
-         int secondTest = second(muscleMass);
-         int thirdTest = third(muscleMass, secondTest);
-
-         String out = muscleMass + " " + secondTest + " " + thirdTest;
-         String category = category(thirdTest);
-
-         System.out.println(out);
-         System.out.println(category);
-     }
- }
- */

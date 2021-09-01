a83f4405844d537a8116542d4880942313adc11a
David Barbosa <david.barbosa@mercadolibre.com.co>
2021-08-31 20:20:38 -0500

    Update Task4.sql

--- Java/src/tasks/Task5/sql/Task4.sql
+++ Java/src/tasks/Task5/sql/Task4.sql
@@ -9,7 +9,6 @@ DROP SCHEMA IF EXISTS `Task4`;
 CREATE SCHEMA IF NOT EXISTS `Task4` DEFAULT CHARACTER SET utf8;
 USE `Task4`;
 
-
 -- -----------------------------------------------------
 -- Table `Task4`.`Bodega`
 -- -----------------------------------------------------
@@ -21,11 +20,6 @@ CREATE TABLE IF NOT EXISTS `Task4`.`Bodega` (
   PRIMARY KEY (`idBodega`)
 );
   
-INSERT INTO `bodega` (`nombre`, `direccion`) VALUES ("la amistad", "Cra 78 # 85 -45");
-INSERT INTO `bodega` (`nombre`, `direccion`) VALUES ("santa isabel", "Calle 34 # 45 -85");
-INSERT INTO `bodega` (`nombre`, `direccion`) VALUES ("santa cecilia", "Cra 89 # 54 - 39"); 
-
-
 -- -----------------------------------------------------
 -- Table `Task4`.`producto`
 -- -----------------------------------------------------
@@ -36,19 +30,6 @@ CREATE TABLE IF NOT EXISTS `Task4`.`producto` (
   `precio` DECIMAL(8,2) NOT NULL,
   PRIMARY KEY(`idproducto`)
 );
-  
-INSERT INTO `producto` (`nombre`, `precio`) VALUES ("tornillo el general", 93277);
-INSERT INTO `producto` (`nombre`, `precio`) VALUES ("champagne", 15728);
-INSERT INTO `producto` (`nombre`, `precio`) VALUES ("mora", 63781);
-INSERT INTO `producto` (`nombre`, `precio`) VALUES ("cereal", 33796);
-INSERT INTO `producto` (`nombre`, `precio`) VALUES ("garbanzo", 86495);
-INSERT INTO `producto` (`nombre`, `precio`) VALUES ("vino espumoso", 47392);
-INSERT INTO `producto` (`nombre`, `precio`) VALUES ("arroz", 67370);
-INSERT INTO `producto` (`nombre`, `precio`) VALUES ("aguardiente", 69188);
-INSERT INTO `producto` (`nombre`, `precio`) VALUES ("condimentos mixtos", 3025);
-INSERT INTO `producto` (`nombre`, `precio`) VALUES ("cerezas dulces", 96453);
-INSERT INTO `producto` (`nombre`, `precio`) VALUES ("aguardiente", 46004);
-
 
 -- -----------------------------------------------------
 -- Table `Task4`.`almacena`
@@ -64,18 +45,6 @@ CREATE TABLE IF NOT EXISTS `Task4`.`almacena` (
   FOREIGN KEY (`idproducto`) REFERENCES `producto`(`idproducto`)
 );
 
-INSERT INTO `almacena` (`idBodega`, `idproducto`, `cantidad`) VALUES ((SELECT `idBodega` FROM `Bodega` WHERE `nombre` = "la amistad"), (SELECT `idproducto` FROM `producto` WHERE `nombre` = "tornillo el general"), 65);
-INSERT INTO `almacena` (`idBodega`, `idproducto`, `cantidad`) VALUES ((SELECT `idBodega` FROM `Bodega` WHERE `nombre` = "la amistad"), (SELECT `idproducto` FROM `producto` WHERE `nombre` = "cereal"), 11);
-INSERT INTO `almacena` (`idBodega`, `idproducto`, `cantidad`) VALUES ((SELECT `idBodega` FROM `Bodega` WHERE `nombre` = "la amistad"), (SELECT `idproducto` FROM `producto` WHERE `nombre` = "cerezas dulces"), 48);
-INSERT INTO `almacena` (`idBodega`, `idproducto`, `cantidad`) VALUES ((SELECT `idBodega` FROM `Bodega` WHERE `nombre` = "santa isabel"), (SELECT `idproducto` FROM `producto` WHERE `nombre` = "champagne"), 63);
-INSERT INTO `almacena` (`idBodega`, `idproducto`, `cantidad`) VALUES ((SELECT `idBodega` FROM `Bodega` WHERE `nombre` = "santa isabel"), (SELECT `idproducto` FROM `producto` WHERE `nombre` = "mora"), 35);
-INSERT INTO `almacena` (`idBodega`, `idproducto`, `cantidad`) VALUES ((SELECT `idBodega` FROM `Bodega` WHERE `nombre` = "santa isabel"), (SELECT `idproducto` FROM `producto` WHERE `nombre` = "vino espumoso"), 85);
-INSERT INTO `almacena` (`idBodega`, `idproducto`, `cantidad`) VALUES ((SELECT `idBodega` FROM `Bodega` WHERE `nombre` = "santa cecilia"), (SELECT `idproducto` FROM `producto` WHERE `nombre` = "champagne"), 11);
-INSERT INTO `almacena` (`idBodega`, `idproducto`, `cantidad`) VALUES ((SELECT `idBodega` FROM `Bodega` WHERE `nombre` = "santa cecilia"), (SELECT `idproducto` FROM `producto` WHERE `nombre` = "mora"), 13);
-INSERT INTO `almacena` (`idBodega`, `idproducto`, `cantidad`) VALUES ((SELECT `idBodega` FROM `Bodega` WHERE `nombre` = "santa cecilia"), (SELECT `idproducto` FROM `producto` WHERE `nombre` = "arroz"), 19);
-INSERT INTO `almacena` (`idBodega`, `idproducto`, `cantidad`) VALUES ((SELECT `idBodega` FROM `Bodega` WHERE `nombre` = "santa cecilia"), (SELECT `idproducto` FROM `producto` WHERE `nombre` = "aguardiente" LIMIT 1), 24);
-
-
 -- -----------------------------------------------------
 -- Table `Task4`.`empleado`
 -- -----------------------------------------------------
@@ -89,6 +58,26 @@ CREATE TABLE IF NOT EXISTS `Task4`.`empleado` (
   FOREIGN KEY (`idBodega`) REFERENCES `Bodega`(`idBodega`)
 );
 
+
+-- -----------------------------------------------------
+-- Inserts
+-- -----------------------------------------------------
+INSERT INTO `bodega` (`nombre`, `direccion`) VALUES ("la amistad", "Cra 78 # 85 -45");
+INSERT INTO `bodega` (`nombre`, `direccion`) VALUES ("santa isabel", "Calle 34 # 45 -85");
+INSERT INTO `bodega` (`nombre`, `direccion`) VALUES ("santa cecilia", "Cra 89 # 54 - 39"); 
+
+INSERT INTO `producto` (`nombre`, `precio`) VALUES ("tornillo el general", 93277);
+INSERT INTO `producto` (`nombre`, `precio`) VALUES ("champagne", 15728);
+INSERT INTO `producto` (`nombre`, `precio`) VALUES ("mora", 63781);
+INSERT INTO `producto` (`nombre`, `precio`) VALUES ("cereal", 33796);
+INSERT INTO `producto` (`nombre`, `precio`) VALUES ("garbanzo", 86495);
+INSERT INTO `producto` (`nombre`, `precio`) VALUES ("vino espumoso", 47392);
+INSERT INTO `producto` (`nombre`, `precio`) VALUES ("arroz", 67370);
+INSERT INTO `producto` (`nombre`, `precio`) VALUES ("aguardiente", 69188);
+INSERT INTO `producto` (`nombre`, `precio`) VALUES ("condimentos mixtos", 3025);
+INSERT INTO `producto` (`nombre`, `precio`) VALUES ("cerezas dulces", 96453);
+INSERT INTO `producto` (`nombre`, `precio`) VALUES ("aguardiente", 46004);
+
 INSERT INTO `empleado` (`idBodega`, `nombre`, `edad`) VALUES ((SELECT `idBodega` FROM `Bodega` WHERE `nombre` = "la amistad"), "Sylvester Leonard", 24);
 INSERT INTO `empleado` (`idBodega`, `nombre`, `edad`) VALUES ((SELECT `idBodega` FROM `Bodega` WHERE `nombre` = "la amistad"), "Ferdinand Dixon", 23);
 INSERT INTO `empleado` (`idBodega`, `nombre`, `edad`) VALUES ((SELECT `idBodega` FROM `Bodega` WHERE `nombre` = "la amistad"), "Stephen I. Caldwell", 18); 
@@ -99,6 +88,17 @@ INSERT INTO `empleado` (`idBodega`, `nombre`, `edad`) VALUES ((SELECT `idBodega`
 INSERT INTO `empleado` (`idBodega`, `nombre`, `edad`) VALUES ((SELECT `idBodega` FROM `Bodega` WHERE `nombre` = "santa isabel"), "Sebastian C. Fleming", 50); 
 INSERT INTO `empleado` (`idBodega`, `nombre`, `edad`) VALUES ((SELECT `idBodega` FROM `Bodega` WHERE `nombre` = "santa cecilia"), "Leila Huffman", 34); 
 INSERT INTO `empleado` (`idBodega`, `nombre`, `edad`) VALUES ((SELECT `idBodega` FROM `Bodega` WHERE `nombre` = "santa cecilia"), "Colby M. Stephenson", 31); 
+
+INSERT INTO `almacena` (`idBodega`, `idproducto`, `cantidad`) VALUES ((SELECT `idBodega` FROM `Bodega` WHERE `nombre` = "la amistad"), (SELECT `idproducto` FROM `producto` WHERE `nombre` = "tornillo el general"), 65);
+INSERT INTO `almacena` (`idBodega`, `idproducto`, `cantidad`) VALUES ((SELECT `idBodega` FROM `Bodega` WHERE `nombre` = "la amistad"), (SELECT `idproducto` FROM `producto` WHERE `nombre` = "cereal"), 11);
+INSERT INTO `almacena` (`idBodega`, `idproducto`, `cantidad`) VALUES ((SELECT `idBodega` FROM `Bodega` WHERE `nombre` = "la amistad"), (SELECT `idproducto` FROM `producto` WHERE `nombre` = "cerezas dulces"), 48);
+INSERT INTO `almacena` (`idBodega`, `idproducto`, `cantidad`) VALUES ((SELECT `idBodega` FROM `Bodega` WHERE `nombre` = "santa isabel"), (SELECT `idproducto` FROM `producto` WHERE `nombre` = "champagne"), 63);
+INSERT INTO `almacena` (`idBodega`, `idproducto`, `cantidad`) VALUES ((SELECT `idBodega` FROM `Bodega` WHERE `nombre` = "santa isabel"), (SELECT `idproducto` FROM `producto` WHERE `nombre` = "mora"), 35);
+INSERT INTO `almacena` (`idBodega`, `idproducto`, `cantidad`) VALUES ((SELECT `idBodega` FROM `Bodega` WHERE `nombre` = "santa isabel"), (SELECT `idproducto` FROM `producto` WHERE `nombre` = "vino espumoso"), 85);
+INSERT INTO `almacena` (`idBodega`, `idproducto`, `cantidad`) VALUES ((SELECT `idBodega` FROM `Bodega` WHERE `nombre` = "santa cecilia"), (SELECT `idproducto` FROM `producto` WHERE `nombre` = "champagne"), 11);
+INSERT INTO `almacena` (`idBodega`, `idproducto`, `cantidad`) VALUES ((SELECT `idBodega` FROM `Bodega` WHERE `nombre` = "santa cecilia"), (SELECT `idproducto` FROM `producto` WHERE `nombre` = "mora"), 13);
+INSERT INTO `almacena` (`idBodega`, `idproducto`, `cantidad`) VALUES ((SELECT `idBodega` FROM `Bodega` WHERE `nombre` = "santa cecilia"), (SELECT `idproducto` FROM `producto` WHERE `nombre` = "arroz"), 19);
+INSERT INTO `almacena` (`idBodega`, `idproducto`, `cantidad`) VALUES ((SELECT `idBodega` FROM `Bodega` WHERE `nombre` = "santa cecilia"), (SELECT `idproducto` FROM `producto` WHERE `nombre` = "aguardiente" LIMIT 1), 24);
 
 
 -- -----------------------------------------------------
